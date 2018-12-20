57b122c054ae844f8855935227ab95bd5b77b7c3
Javier Martínez <jma96.elx@gmail.com>
2018-12-21 00:55:17 +0100

    Arreglado core puntos de xp

--- Assets/Models/Field Objects/POI XP Bonus/FloatAndRotate.cs
+++ Assets/Models/Field Objects/POI XP Bonus/FloatAndRotate.cs
@@ -4,29 +4,5 @@ using UnityEngine;
 
 public class FloatAndRotate : MonoBehaviour {
 
-    [SerializeField] private float rotateSpeed = 50f;
-    [SerializeField] private float floatAmplitude = 1f;
-    [SerializeField] private float floatFrequency = 1f;
 
-    private Vector3 startPos;
-
-    void Start()
-    {
-        startPos = transform.position;
-    }
-
-    // Update is called once per frame
-    void Update () {
-        if (gameObject !=null && transform != null)
-        {
-            transform.Rotate(Vector3.up, rotateSpeed * Time.deltaTime);
-            Vector3 tempPos = startPos;
-            tempPos.y += Mathf.Sin(Time.fixedTime * Mathf.PI * floatFrequency) * floatAmplitude;
-            transform.position = tempPos;
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
-	}
 }
--- Assets/Models/Field Objects/POI XP Bonus/XPBonus.cs
+++ Assets/Models/Field Objects/POI XP Bonus/XPBonus.cs
@@ -5,10 +5,42 @@ using UnityEngine;
 public class XPBonus : MonoBehaviour {
 
     private int bonus = 40;
+    [SerializeField] private float rotateSpeed = 50f;
+    [SerializeField] private float floatAmplitude = 1f;
+    [SerializeField] private float floatFrequency = 1f;
+
+    private Vector3 startPos;
+    Renderer rend;
+
+    void Start()
+    {
+        startPos = transform.position;
+        rend = GetComponent<Renderer>();
+
+        //DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.gameObject != null && this.transform != null && this.enabled)
+        {
+            transform.Rotate(Vector3.up, rotateSpeed * Time.deltaTime);
+            Vector3 tempPos = startPos;
+            tempPos.y += Mathf.Sin(Time.fixedTime * Mathf.PI * floatFrequency) * floatAmplitude;
+            transform.position = tempPos;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
 
     private void OnMouseDown()
     {
         GameManager.Instance.CurrentPlayer.AddXp(bonus);
-        Destroy(this.gameObject);
+        this.enabled = false;
+        rend.enabled = false;
+
     }
 }
