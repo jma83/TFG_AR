9f71dfdd20e4af8c7a19973423a44f2935ce7615
Javier Martínez <jma96.elx@gmail.com>
2019-01-17 01:13:32 +0100

    Starting Inventory

---
+++ Assets/Scenes/World/Inventory.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour {
+
+    [SerializeField] private GameObject slotContainer;
+
+    private bool inventoyEnabled;
+
+    private int allSlots;
+    private int enabledSlots;
+    private GameObject[] slot;
+
+    
+
+    public void Start()
+    {
+        allSlots = 24;
+        slot = new GameObject[allSlots];
+        for (int i = 0; i < allSlots; i++)
+        {
+            slot[i] = slotContainer.transform.GetChild(i).gameObject;
+        }
+    }
+
+}
--- Assets/Scenes/World/UIManager.cs
+++ Assets/Scenes/World/UIManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Assertions;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour {
@@ -11,9 +12,13 @@ public class UIManager : MonoBehaviour {
     [SerializeField] private Text xpText;
     [SerializeField] private Text levelText;
     [SerializeField] private GameObject menu;
+    private Button[] menu_sections;
     [SerializeField] private AudioClip menuButtonSound;
+    [SerializeField] private GameObject inventory;
 
     private AudioSource audioSource;
+    private int menuSectionCont=3;
+    private bool check = false;
 
     private void Awake()
     {
@@ -23,7 +28,15 @@ public class UIManager : MonoBehaviour {
         Assert.IsNotNull(xpText);
         Assert.IsNotNull(levelText);
         Assert.IsNotNull(menu);
-        Assert.IsNotNull(menuButtonSound); 
+        Assert.IsNotNull(menuButtonSound);
+
+        menuSectionCont = menu.transform.childCount;
+        menu_sections = new Button[menuSectionCont];
+
+        for (int i = 0; i < menuSectionCont; i++)
+        {
+            menu_sections[i] = menu.transform.GetChild(i).gameObject.GetComponent<Button>();
+        }
     }
 
     private void Update()
@@ -31,6 +44,9 @@ public class UIManager : MonoBehaviour {
         updateLevel();
         updateXP();
         updateHP();
+        check = false;
+        //menu_sections[0].onClick.AddListener(toggleInventory);
+        //menu_sections[0].onClick.AddListener(toggleMenu);
     }
 
     public void updateLevel() {
@@ -50,14 +66,26 @@ public class UIManager : MonoBehaviour {
         currentHPBar.transform.localScale = new Vector3(f, 1, 1);
     }
 
-    public void MenuButtonClick()
+    public void MenuButtonClick(int a)
     {
+        
         audioSource.PlayOneShot(menuButtonSound);
-      //  toggleMenu();
+        if (a == 0)
+            toggleMenu();
+        else
+            toggleInventory();
     }
 
     private void toggleMenu()
     {
         menu.SetActive(!menu.activeSelf);
     }
+    private void toggleInventory()
+    {
+
+        inventory.SetActive(!inventory.activeSelf);
+        check = true;
+    }
+
+
 }
